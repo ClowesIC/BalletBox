33f0661dcf40d7ff26bc241bb102579da76fbb2b
ClowesIC <ian102505@gmail.com>
2026-01-08 14:51:49 -0500

    Add files via upload
    My first lab using java in my Software Engineering class. This lab involves developing my first ever package, as well as using java docs for the first time using Intellij as the chosen IDE.

---
+++ W1-Lab-Clowes/src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * HelloWorld.java:
+ * a class to print out Hello World
+ *
+ * @author Ian Clowes
+ *
+ */
+
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+    }
+}
---
+++ W1-Lab-Clowes/src/MyFirstPackage/PackageClass.java
@@ -0,0 +1,20 @@
+package MyFirstPackage;
+/**
+ * PackageClass.java:
+ * A class to print out some information from within my first package.
+ *
+ * @author Ian Clowes
+ */
+public class PackageClass {
+    /**
+     * Main begins with program execution
+     *
+     * @param args Command line argument
+     */
+    public static void main(String[] args){
+        System.out.println("This is my first program using packages!");
+        int x = 2+7;
+        System.out.println("2 + 7 = " + x);
+
+    }
+}
